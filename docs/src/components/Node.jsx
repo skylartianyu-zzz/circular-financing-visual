fab808c3e68ee8e06a7f0c6790f7123f74f5d3cd
skylartianyu-zzz <skylar.tianyu@gmail.com>
2025-12-14 16:39:12 -0500

    Update Node.jsx

--- docs/src/components/Node.jsx
+++ docs/src/components/Node.jsx
@@ -1,1 +1,27 @@
+import React, { useRef } from "react";
+import { Html } from "@react-three/drei";
+import * as THREE from "three";
+
+export default function Node({ node }) {
+  const ref = useRef();
+
+  return (
+    <mesh ref={ref} position={[node.x, node.y, node.z]}>
+      {/* Sphere for company node */}
+      <sphereGeometry args={[node.size || 3, 32, 32]} />
+      <meshStandardMaterial
+        color={"#0a84ff"}
+        emissive={"#003d99"}
+        emissiveIntensity={0.4}
+        metalness={0.3}
+        roughness={0.2}
+      />
+
+      {/* Floating text label */}
+      <Html position={[0, node.size + 2, 0]} center style={{ color: "white" }}>
+        {node.id}
+      </Html>
+    </mesh>
+  );
+}
 
