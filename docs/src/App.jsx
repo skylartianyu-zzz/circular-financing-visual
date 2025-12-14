34b9c36ccdc50a55c4b1113e2e565a496a99f22c
skylartianyu-zzz <skylar.tianyu@gmail.com>
2025-12-14 16:38:33 -0500

    Update App.jsx

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import Scene from "./components/Scene";
+import "./styles.css"; // optional if you want global css
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", background: "#050607" }}>
+      <Scene />
+    </div>
+  );
+}
 
