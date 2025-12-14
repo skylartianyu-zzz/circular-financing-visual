5b26855b4c3f1741b44655f7ad6059cc81d66783
skylartianyu-zzz <skylar.tianyu@gmail.com>
2025-12-14 16:39:59 -0500

    Update uiState.js

--- docs/src/state/uiState.js
+++ docs/src/state/uiState.js
@@ -1,1 +1,29 @@
+import { create } from "zustand";
+
+const useUIState = create((set) => ({
+  hoveredEdge: null,
+  clickedEdge: null,
+
+  setHoveredEdge: (edge) => set({ hoveredEdge: edge }),
+  setClickedEdge: (edge) => {
+    if (!edge) return set({ clickedEdge: null });
+
+    // attach mouse position for positioning the SnippetBox
+    set({
+      clickedEdge: {
+        ...edge,
+        mouseX: window.lastPointerX || 200,
+        mouseY: window.lastPointerY || 200,
+      },
+    });
+  },
+}));
+
+// Track mouse globally so snippet box knows where to render
+window.addEventListener("pointermove", (e) => {
+  window.lastPointerX = e.clientX;
+  window.lastPointerY = e.clientY;
+});
+
+export default useUIState;
 
